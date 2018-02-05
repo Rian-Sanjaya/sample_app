646140e5d6aa8b9c92122417382a909aeb728872
Rian <rian.sj@gmail.com>
2018-02-05 09:28:34 +0000

    Add more users controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
   	@user = User.new
   end
@@ -6,12 +10,16 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		redirect_to static_pages_home_path
+  		redirect_to users_path
   	else
   		render "new"
   	end
   end
 
+  def show
+    @user = User.find(params[:id])  
+  end
+  
   private
 
   	def user_params
